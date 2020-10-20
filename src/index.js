ee8d14e6aa545a91a34816bb14dd868754a1e9b0
bwnstk <info@flimmerkiste.xyz>
2020-10-20 12:48:11 +0200

    Timer is Ready to count till minus ~

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import "./style.css";
 import { createElement } from "./utils/elements";
 
+var startInterval;
 let counter = 400;
+
+function startTimer() {
+  if (!startInterval) {
+    startInterval = setInterval(countDownOne, 100);
+  }
+}
+
+function stopTimer() {
+  clearInterval(startInterval);
+}
 
 function countDownOne() {
   counter--;
   const counterNumber = document.querySelector(".timeOutput");
-  counterNumber.innerHTML = counter;
+  counterNumber.innerHTML = `${counter}ms`;
   console.log(counterNumber.innerHTML);
 }
 
 function component() {
   const timeOutput = createElement("span", {
     className: "timeOutput",
-    innerText: counter,
+    innerHTML: `${counter}ms`,
   });
 
   const timeInput = createElement("input", {
@@ -21,17 +32,25 @@ function component() {
     placeholder: "hh:mm:ss",
   });
 
-  const timerButton = createElement("button", {
+  const timerButtonStart = createElement("button", {
     className: "button--startCount",
-    innerText: "Count",
+    innerText: "Count Down",
     onclick: () => {
-      countDownOne();
-      console.log("counter", counter);
+      startTimer();
     },
   });
+
+  const timerButtonStop = createElement("button", {
+    className: "button--startCount",
+    innerText: "Stop",
+    onclick: () => {
+      stopTimer();
+    },
+  });
+
   const Container = createElement("div", {
     className: "container",
-    children: [timeInput, timerButton, timeOutput],
+    children: [timeInput, timerButtonStart, timerButtonStop, timeOutput],
   });
 
   return Container;
